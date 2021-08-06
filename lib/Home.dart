1b03075f13a260274def78a47d753116e13c8b3c
Gajjar Mohit <debugntitylab.io@gmail.com>
2021-08-06 18:47:59 +0530

    Added the code

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,3 @@
-import 'package:ceasar_cipher/CeasarCipher_Algo.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,13 +11,132 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-          },
-          child: Text("Press"),
-        ),
-      ),
-    );
+        backgroundColor: Color(0xff000000),
+        body: Container(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Caesar",
+                        style: TextStyle(color: Color(0xff707070), fontSize: 37, fontFamily: 'OCR')),
+                    Text("Cipher",
+                        style: TextStyle(color: Color(0xff707070), fontSize: 37, fontFamily: 'OCR')),
+                    Text("Algorithm",
+                        style: TextStyle(color: Color(0xff707070), fontSize: 37, fontFamily: 'OCR')),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xff18181A)),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 5),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.text_format, color: Color(0xff707070),),
+                        hintText: "Paste the Text",
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(
+                          fontFamily: 'OCR',
+                          color: Color(0xff707070),
+                          fontSize: 23
+                        )
+                      ),
+                       style: TextStyle(color: Color(0xff707070), fontSize: 23, fontFamily: 'OCR')
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xff18181A)),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 5),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.vpn_key, color: Color(0xff707070),),
+                        hintText: "Enter Key",
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(
+                          fontFamily: 'OCR',
+                          color: Color(0xff707070),
+                          fontSize: 23
+                        ),
+                      ),
+                      style: TextStyle(color: Color(0xff707070), fontSize: 23, fontFamily: 'OCR')
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                     border: Border.all(
+                       width: 3,
+                      color: Color(0xff18181A)
+                     )
+                      ),
+                  child: Center(
+                    child: Text(
+                      "Encrypt Text",
+                       style: TextStyle(color: Color(0xff707070), fontSize: 23, fontFamily: 'OCR')),
+                    ),
+                ),
+                SizedBox(height: 20,),
+                 Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                     border: Border.all(
+                       width: 3,
+                      color: Color(0xff18181A)
+                     )
+                      ),
+                  child:Center(
+                    child: Text(
+                      "Decrypt Text",
+                       style: TextStyle(color: Color(0xff707070), fontSize: 23, fontFamily: 'OCR')),
+                    ),
+                  ),
+                  SizedBox(height:20),
+                  Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                      color: Color(0xff18181A)
+                      ),
+                  child:Padding(
+                    padding: EdgeInsets.only(left: 15, top: 10),
+                    child: Text(
+                      "Decrypt Text:",
+                       style: TextStyle(color: Color(0xff707070), fontSize: 23, fontFamily: 'OCR')),
+                    ),
+                  ),
+                  SizedBox(height:10),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Text("Share the Text",style: TextStyle(color: Color(0xff707070), fontSize: 23, fontFamily: 'OCR')),
+                 )
+              
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Caesar Cipher',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
